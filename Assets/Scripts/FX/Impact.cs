1aa63e2d6c3f9949fbfbda99678162eea74c126e
niharkanta1 <niharkanta1@live.com>
2023-01-14 13:39:21 +0530

    Impact Effects

---
+++ Assets/Scripts/FX/Impact.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*=============================================
+Product:    Juicy2DShooter v1.0
+Developer:  nihar
+Company:    DeadW0Lf Games
+Date:       14-01-2023 12:57:36
+================================================*/ 
+public class Impact : MonoBehaviour {
+
+    public void DestroyAfterAnimation() {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Weapons/BulletRegular.cs
+++ Assets/Scripts/Weapons/BulletRegular.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 /*=============================================
 Product:    Juicy2DShooter v1.0
 Developer:  nihar
 Company:    DeadW0Lf Games
 Date:       05-01-2023 12:42:34
-================================================*/ 
+================================================*/
 public class BulletRegular : Bullet {
     protected Rigidbody2D rb2d;
 
@@ -30,20 +31,24 @@ public class BulletRegular : Bullet {
     private void OnTriggerEnter2D(Collider2D collision) {
         
         if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacle")) {
-            HitObstacle();
+            HitObstacle(collision);
         } else if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy")) {
             var hittable = collision.GetComponent<IHittable>();
             hittable?.GetHit(bulletData.Damage, gameObject);
-            HitEnemy();
+            HitEnemy(collision);
         }
         Destroy(gameObject);
     }
 
-    private void HitEnemy() {
-        
+    private void HitEnemy(Collider2D collision) {
+        Vector2 randomOffset = Random.insideUnitCircle * 0.5f;
+        Instantiate(BulletData.ImpactEnemyPrefab, collision.transform.position + (Vector3)randomOffset, Quaternion.identity);
     }
 
-    private void HitObstacle() {
-        
+    private void HitObstacle(Collider2D collision) {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right);
+        if(hit.collider != null) {
+            Instantiate(BulletData.ImpactObstaclePrefab, hit.point, Quaternion.identity);
+        }
     }
 }
